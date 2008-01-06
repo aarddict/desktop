4381982630bef12c97627f30f6d57bd1f3c7d0b2
Jeremy Mortis <mortis@ucalgary.ca>
2008-01-06 16:28:27 -0700

    Rewritten for aarddict

---
+++ src/article.py
@@ -0,0 +1,162 @@
+#!/usr/bin/python
+
+"""
+This file is part of Aarddict Dictionary Viewer
+(http://code.google.com/p/aarddict) 
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (C) 2008  Jeremy Mortis and Igor Tkach
+"""
+
+import os
+from sgmllib import SGMLParser
+import simplejson
+import bz2
+import struct
+import sys
+
+class Article:
+
+    def __init__(self, title = "", text = "", tags = [], compress = "none"):
+        self.title = title
+        self.text = text
+        self.tags = tags
+        self.compress = compress
+
+    def __str__(self):
+        s = "title: " + self.title + "\n"
+        s = s + "text: " + self.text + "\n"
+        for tag in self.tags:
+            s = s + "tag: " + str(tag) + "\n"
+        return s
+
+    def fromHTML(self, string):
+        self.text = ""
+        self.tags = []
+        p = ArticleTagParser(self)
+        p.feed(string)
+        if __debug__:
+            f = open("debug.html", "w")
+            f.write(string)
+            f.close()
+        
+    def fromJSON(self,string):
+        self.text, tagList = simplejson.loads(string)
+        self.tags = []
+        for tagListItem in tagList:
+            tag = Tag()
+            tag.fromList(tagListItem)
+            self.tags.append(tag)
+        
+    def fromFile(self, file, offset):
+        file.seek(offset)
+        record_length = struct.unpack('L', file.read(struct.calcsize("L")))[0]
+        #sys.stderr.write("record length, offset: " + str(record_length) + " " + str(offset) + "\n")
+        if self.compress == "bz2":
+            s = file.read(record_length)
+        else:
+            s = bz2.decompress(file.read(record_length))
+
+        self.fromJSON(s)
+
+    def toJSON(self):
+        tagList = []
+        for tag in self.tags:
+            tagList.append(tag.toList())
+        return simplejson.dumps([self.text, tagList])
+
+    def toFile(self, file):
+        if self.compress == "bz2":
+            s = bz2.compress(self.toJSON())
+        else:
+            s = self.toJSON()
+
+        #sys.stderr.write("write article: " + file.tell() + " " + len(s) + " " + self.title)
+        file.write(struct.pack("L", len(s)) + s)
+        return struct.calcsize("L") + len(s)
+
+class Tag:
+
+    def __init__(self, name = "", start = -1, end = -1, attributes = {}):
+        self.name = name
+        self.start = start
+        self.end = end
+        self.attributes = attributes
+
+    def __str__(self):
+        return self.name + " " + str(self.start) + " " +  str(self.end) + " " + str(self.attributes)
+    
+    def toList(self):
+        return [self.name, self.start, self.end, self.attributes]
+
+    def fromList(self, list):
+        self.name, self.start, self.end, self.attributes = list
+
+    
+class ArticleTagParser(SGMLParser):
+
+    def __init__(self, article):
+        SGMLParser.__init__(self)
+        self.article = article
+        self.tagstack = []
+
+    def do_p(self, attrsList):
+        self.article.text = self.article.text + "\n\n"
+
+    def do_br(self, attrsList):
+        self.article.text = self.article.text + "\n"
+    
+    def unknown_starttag(self, tag, attrsList):
+
+        attrsDict = {}
+        attrsDict.update(attrsList)
+            
+        t = Tag(tag, len(self.article.text), -1, attrsDict)
+        self.tagstack.append(t)
+
+    def unknown_endtag(self, tag):
+
+        if (len(self.tagstack) == 0) or (self.tagstack[-1].name != tag):
+            sys.stderr.write("Mismatched end tag: </" + tag + "> in " + self.article.title + " at \"" + self.article.text[-20:] + "\"\n")
+            return
+        t = self.tagstack.pop()
+        t.end = len(self.article.text) - 1
+        self.article.tags.append(t)
+
+    def handle_data(self, data):
+
+        self.article.text = self.article.text + data
+
+if __name__ == '__main__':
+    import sys
+
+    s = '<title>This is a title</title><br>\n<a href="there">this <i>and</i> <b>that</b></a>'
+    article = Article()
+
+    article.fromHTML(s)
+
+    print article
+
+    json = article.toJSON()
+    print json
+    
+    article.fromJSON(json)
+    print article
+
+    print "Done."
+
+
+
+
+
---
+++ src/dictionary.py
@@ -0,0 +1,191 @@
+#!/usr/bin/python
+
+"""
+This file is part of Aarddict Dictionary Viewer
+(http://code.google.com/p/aarddict)
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (C) 2008  Jeremy Mortis and Igor Tkach
+"""
+
+from struct import unpack
+import types
+import simplejson
+import article
+import pyuca
+import struct
+import sys
+
+class Word:
+    def __init__(self, dictionary, word, collationKey = None):
+        self.dictionary = dictionary
+        self.encoding = dictionary.metadata.get("character_encoding", "utf-8")
+        self.collator = dictionary.collator
+        self.article_ptr = None
+        self.unicode = None
+        self.word = None
+        self.collationKey = collationKey
+        self.word_lang = dictionary.metadata.get("index_language", "en-US")
+        self.article_lang = dictionary.metadata.get("article_language", "en-US")
+        
+        if type(word) is types.UnicodeType:
+            self.unicode = word
+            self.word = self.unicode.encode(self.encoding)
+        else:
+            self.word = word
+            try:
+                self.unicode = self.word.decode(self.encoding)
+            except UnicodeDecodeError:
+                self.unicode = u"error"
+                sys.stderr.write("Unable to decode: " + self.encoding + " " + word + "\n")
+
+        if not self.collationKey: 
+            self.collationKey = self.dictionary.collator.getCollationKey(self.unicode)
+
+    def __str__(self):
+        return self.word
+
+    def __eq__(self, other):
+        return self.collationKey == other.collationKey
+
+    def __cmp__(self, other):
+        return cmp(self.collationKey, other.collationKey)
+
+    def __unicode__(self):
+        return self.unicode
+        
+    def startswith(self, other):
+        return self.collationKey.startswith(other.collationKey)
+    
+    def getArticle(self):
+        return self.dictionary.readArticle(self.article_ptr)
+        
+class Dictionary:         
+    
+    def __init__(self, file_name, collator):    
+        self.file_name = file_name
+        self.file = open(file_name, "rb");
+        self.fileid = self.file.read(5);
+        if self.fileid != "pdi10":
+            raise Exception(file_name + " is not a recognized pdi file")
+        self.metadataLength = int(self.file.read(8));
+        self.metadataString = self.file.read(self.metadataLength)
+        self.metadata = simplejson.loads(self.metadataString)
+        self.collator = collator
+        self.word_list = None
+        self.article_offset = self.metadata["article_offset"]
+        self.index_start = self.metadata["index_offset"]
+        self.index_end = self.index_start + self.metadata["index_length"] - 1
+
+    def __eq__(self, other):
+        return self.key() == other.key()
+    
+    def __str__(self):
+        return self.file_name
+    
+    def __hash__(self):
+        return self.key().__hash__()
+
+    def key(self):
+        return (self.metadata["title"], self.metadata.get["pdi_version"], self.file_name)
+       
+    def find_index_entry(self, word):
+        low = self.index_start
+        high = self.index_end
+        probe = -1
+        while True:
+            prevprobe = probe
+            probe = low + int((high-low)/2)
+            probe = self.findword(probe)
+            if probe == prevprobe:
+                return low
+            next_offset, probeword = self.read_full_index_item(probe)
+            #sys.stderr.write("probe: " + probeword + "\n")
+            if probeword == word:
+                return probe
+            if probeword > word:
+                high = probe
+            else:
+                low = probe
+
+    def findword(self, pos):
+        self.file.seek(pos)
+        b = ""
+        start = -1
+        while (start == -1) and (pos + len(b) < self.index_end):
+            b = ''.join([b, self.file.read(128)])
+            start = b.find("\xFD\xFD\xFD\xFD")
+        if start == -1:
+            raise Exception("could not find start position in long index: " + str(pos))
+        return pos + start
+
+    
+    def get_word_list_iter(self, start_string):
+        start_word = Word(self, start_string)
+        next_ptr = self.find_index_entry(start_word)
+        found = False 
+        while True:
+            if next_ptr < 0:
+                raise StopIteration
+            next_offset, word = self.read_full_index_item(next_ptr)
+            #sys.stderr.write("Word: " + word + "\n")
+            if word.startswith(start_word):
+                found = True
+                yield word
+            else:
+                #sys.stderr.write("Tossed: " + word + "\n")
+                if word > start_word:
+                    raise StopIteration
+            next_ptr += next_offset
+
+                
+    def read_full_index_item(self, pointer):
+        f = self.file
+        f.seek(pointer)
+        sep = f.read(4)
+        headerpack = 'LLhL'
+        header_length = struct.calcsize(headerpack)
+        s = f.read(header_length)
+        next_word_offset, prev_word_offset, fileno, article_ptr = struct.unpack(headerpack, s)
+        s = f.read(next_word_offset - header_length - 4)
+        key, word = s.split("___", 2)
+        #sys.stderr.write("next,prev,file,offset: " + str(next_word_offset) + " " + str(prev_word_offset) + " " + str(fileno) + " " + str(article_ptr) + " " repr(word) = "\n")
+            
+        word = Word(self, word)
+        word.article_ptr = article_ptr + self.metadata["article_offset"]
+        return next_word_offset, word
+        
+    def readArticle(self, pointer):
+        a = article.Article()
+        a.fromFile(self.file, pointer)
+        return a
+
+    def close(self, save_index = True):
+        self.file.close()        
+
+if __name__ == '__main__':
+
+        import sys
+
+        if len(sys.argv) != 3:
+            sys.stderr.write("Usage: " + sys.argv[0] + " aarfile word\n")
+            sys.exit()
+        
+        collator1 = pyuca.Collator("allkeys.txt", strength = 1)
+        d = Dictionary(sys.argv[1], collator1)
+
+        i = d.get_word_list_iter(sys.argv[2])
+        for word in i:
+            print word, "==>",  word.getArticle().toJSON()
+
+        print "Done."
