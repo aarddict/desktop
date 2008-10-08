30fcafe5ba40a3007d5bf3519067f583e750b148
Igor Tkach <itkach@aarddict.org>
2008-10-07 23:27:57 -0400

    Moved main code to if __name__ == '__main__': block

--- tools/compiler.py
+++ tools/compiler.py
@@ -171,214 +171,213 @@ def makeFullIndex():
     
     sys.stderr.write("\r" + str(count) + "\n")
 
-#__main__
-
-tempDir = tempfile.mkdtemp()
-root_locale = Locale('root')
-collator4 =  Collator.createInstance(root_locale)
-collator4.setStrength(4)
-
-collator1 =  Collator.createInstance(root_locale)
-collator1.setStrength(1)
-
-sortex = SortExternal()
-
-header = {
-    "character_encoding": "utf-8",
-    "compression_type": "bz2",
-    "major_version": 1,
-    "minor_version": 0,
-    "timestamp": str(datetime.datetime.utcnow()),
-    "file_sequence": 0,
-    "article_language": "en",
-    "index_language": "en"
-    }
-
-sys.stderr.write("Parsing input file...\n")
-
-optionsParser = getOptions()
-options, args = optionsParser.parse_args()
-
-if options.input_file:
-    inputFile = open(options.input_file, "rb", 4096)
-else:
-    inputFile = sys.stdin
-
-aarFile = []
-aarFileLength = []
-
-if not options.output_file:
-    optionsParser.print_help()
-    sys.exit()
+if __name__ == '__main__':
+    tempDir = tempfile.mkdtemp()
+    root_locale = Locale('root')
+    collator4 =  Collator.createInstance(root_locale)
+    collator4.setStrength(4)
     
-aarFile.append(open(options.output_file, "w+b", 4096))
-aarFileLength.append(0)
-
-createArticleFile()
-
-aarFileLengthMax = 2000000000
-
-indexDbFullname = os.path.join(tempDir, "index.db")
-indexDb = shelve.open(indexDbFullname, 'n')
-
-if options.templates:
-    templateDb = cdbwiki.WikiDB(options.templates)
-else:
-    templateDb = None
+    collator1 =  Collator.createInstance(root_locale)
+    collator1.setStrength(1)
     
-index1 = tempfile.NamedTemporaryFile()
-index2 = tempfile.NamedTemporaryFile()
-index1Length = 0
-index2Length = 0
-
-articlePointer = 0L
-
-header["article_count"] =  0
-header["index_count"] =  0
-
-if options.input_format == "xdxf" or inputFile.name[-5:] == ".xdxf":
-    sys.stderr.write("Compiling %s as xdxf\n" % inputFile.name)
-    import xdxf
-    p = xdxf.XDXFParser(header, handleArticle)
-    p.parse(inputFile)
-else:  
-    sys.stderr.write("Compiling %s as mediawiki\n" % inputFile.name)
-    from mediawikiparser import MediaWikiParser
-    p = MediaWikiParser(collator1, header, templateDb, handleArticle)
-    p.parseFile(inputFile)
-
-sys.stderr.write("\r" + str(header["article_count"]) + "\n")
-
-sys.stderr.write("Sorting index...\n")
-
-sortex.sort()
-
-sys.stderr.write("Writing temporary indexes...\n")
-
-makeFullIndex()
-
-sortex.cleanup()
-
-indexDb.close()
-os.remove(indexDbFullname)
-os.rmdir(tempDir)
-
-combineFiles = False
-header["file_count"] = len(aarFile)
-if 100 + index1Length + index2Length + aarFileLength[-1] < aarFileLengthMax:
-    header["file_count"] -= 1
-    combineFiles = True
-header["file_count"] = "%06i" % header["file_count"]
-
-sys.stderr.write("Composing header...\n")
-
-f = open("ISO-639-2.txt", "r")
-languageCodeDict = {}
-for line in f:
-    codes = line.split('|')
-    languageCodeDict[codes[0]] = codes[2]
-f.close()
-
-header["article_language"] = languageCodeDict.get(header["article_language"].lower(), header["article_language"])
-header["index_language"] = languageCodeDict.get(header["index_language"].lower(), header["index_language"])
-
-header["index1_length"] = index1Length
-header["index2_length"] = index2Length
-
-header["index1_offset"] = "%012i" % 0
-header["index2_offset"] = "%012i" % 0
-header["article_offset"] = "%012i" % 0
-
-jsonText = compactjson.dumps(header)
-
-header["index1_offset"] = "%012i" % (5 + 8 + len(jsonText))
-header["index2_offset"] = "%012i" % (5 + 8 + len(jsonText) + index1Length)
-header["article_offset"] = "%012i" % (5 + 8 + len(jsonText) + index1Length + index2Length)
-
-sys.stderr.write("Writing header...\n")
-
-jsonText = compactjson.dumps(header)
-
-aarFile[0].write("aar%02i" % header["major_version"])
-aarFileLength[0] += 5
-
-aarFile[0].write("%08i" % len(jsonText))
-aarFileLength[0] += 8
-
-aarFile[0].write(jsonText)
-aarFileLength[0] += len(jsonText)
-
-sys.stderr.write("Writing index 1...\n")
-
-index1.flush()
-index1.seek(0)
-writeCount = 0
-while 1:
-    if writeCount % 100 == 0:
-        sys.stderr.write("\r" + str(writeCount))
-    unit = index1.read(12)
-    if len(unit) == 0:
-        break
-    index2ptr, fileno, offset = struct.unpack(">LLL", unit)
-    if combineFiles and fileno == len(aarFile) - 1:
-        fileno = 0L
-    unit = struct.pack(">LLL", index2ptr, fileno, offset) 
-    writeCount += 1
-    aarFile[0].write(unit)
-    aarFileLength[0] += 12
-sys.stderr.write("\r" + str(writeCount) + "\n")
-index1.close()
-
-sys.stderr.write("Writing index 2...\n")
-
-index2.flush()
-index2.seek(0)
-writeCount = 0
-while 1:
-    if writeCount % 100 == 0:
-        sys.stderr.write("\r" + str(writeCount))
-    unitLengthString = index2.read(4)
-    if len(unitLengthString) == 0:
-        break
-    writeCount += 1
-    unitLength = struct.unpack(">L", unitLengthString)[0]
-    unit = index2.read(unitLength)
-    aarFile[0].write(unitLengthString + unit)
-    aarFileLength[0] += 4 + unitLength
-sys.stderr.write("\r" + str(writeCount) + "\n")
-index2.close()
-
-writeCount = 0L
-
-if combineFiles:
-    sys.stderr.write("Appending %s to %s\n" % (aarFile[-1].name, aarFile[0].name))
-    aarFile[-1].flush()
-    aarFile[-1].seek(0)
-    aarFile[-1].read(5)
-    headerLength = int(aarFile[-1].read(8))
-    aarFile[-1].read(headerLength)
-
+    sortex = SortExternal()
+    
+    header = {
+        "character_encoding": "utf-8",
+        "compression_type": "bz2",
+        "major_version": 1,
+        "minor_version": 0,
+        "timestamp": str(datetime.datetime.utcnow()),
+        "file_sequence": 0,
+        "article_language": "en",
+        "index_language": "en"
+        }
+    
+    sys.stderr.write("Parsing input file...\n")
+    
+    optionsParser = getOptions()
+    options, args = optionsParser.parse_args()
+    
+    if options.input_file:
+        inputFile = open(options.input_file, "rb", 4096)
+    else:
+        inputFile = sys.stdin
+    
+    aarFile = []
+    aarFileLength = []
+    
+    if not options.output_file:
+        optionsParser.print_help()
+        sys.exit()
+        
+    aarFile.append(open(options.output_file, "w+b", 4096))
+    aarFileLength.append(0)
+    
+    createArticleFile()
+    
+    aarFileLengthMax = 2000000000
+    
+    indexDbFullname = os.path.join(tempDir, "index.db")
+    indexDb = shelve.open(indexDbFullname, 'n')
+    
+    if options.templates:
+        templateDb = cdbwiki.WikiDB(options.templates)
+    else:
+        templateDb = None
+        
+    index1 = tempfile.NamedTemporaryFile()
+    index2 = tempfile.NamedTemporaryFile()
+    index1Length = 0
+    index2Length = 0
+    
+    articlePointer = 0L
+    
+    header["article_count"] =  0
+    header["index_count"] =  0
+    
+    if options.input_format == "xdxf" or inputFile.name[-5:] == ".xdxf":
+        sys.stderr.write("Compiling %s as xdxf\n" % inputFile.name)
+        import xdxf
+        p = xdxf.XDXFParser(header, handleArticle)
+        p.parse(inputFile)
+    else:  
+        sys.stderr.write("Compiling %s as mediawiki\n" % inputFile.name)
+        from mediawikiparser import MediaWikiParser
+        p = MediaWikiParser(collator1, header, templateDb, handleArticle)
+        p.parseFile(inputFile)
+    
+    sys.stderr.write("\r" + str(header["article_count"]) + "\n")
+    
+    sys.stderr.write("Sorting index...\n")
+    
+    sortex.sort()
+    
+    sys.stderr.write("Writing temporary indexes...\n")
+    
+    makeFullIndex()
+    
+    sortex.cleanup()
+    
+    indexDb.close()
+    os.remove(indexDbFullname)
+    os.rmdir(tempDir)
+    
+    combineFiles = False
+    header["file_count"] = len(aarFile)
+    if 100 + index1Length + index2Length + aarFileLength[-1] < aarFileLengthMax:
+        header["file_count"] -= 1
+        combineFiles = True
+    header["file_count"] = "%06i" % header["file_count"]
+    
+    sys.stderr.write("Composing header...\n")
+    
+    f = open("ISO-639-2.txt", "r")
+    languageCodeDict = {}
+    for line in f:
+        codes = line.split('|')
+        languageCodeDict[codes[0]] = codes[2]
+    f.close()
+    
+    header["article_language"] = languageCodeDict.get(header["article_language"].lower(), header["article_language"])
+    header["index_language"] = languageCodeDict.get(header["index_language"].lower(), header["index_language"])
+    
+    header["index1_length"] = index1Length
+    header["index2_length"] = index2Length
+    
+    header["index1_offset"] = "%012i" % 0
+    header["index2_offset"] = "%012i" % 0
+    header["article_offset"] = "%012i" % 0
+    
+    jsonText = compactjson.dumps(header)
+    
+    header["index1_offset"] = "%012i" % (5 + 8 + len(jsonText))
+    header["index2_offset"] = "%012i" % (5 + 8 + len(jsonText) + index1Length)
+    header["article_offset"] = "%012i" % (5 + 8 + len(jsonText) + index1Length + index2Length)
+    
+    sys.stderr.write("Writing header...\n")
+    
+    jsonText = compactjson.dumps(header)
+    
+    aarFile[0].write("aar%02i" % header["major_version"])
+    aarFileLength[0] += 5
+    
+    aarFile[0].write("%08i" % len(jsonText))
+    aarFileLength[0] += 8
+    
+    aarFile[0].write(jsonText)
+    aarFileLength[0] += len(jsonText)
+    
+    sys.stderr.write("Writing index 1...\n")
+    
+    index1.flush()
+    index1.seek(0)
+    writeCount = 0
     while 1:
         if writeCount % 100 == 0:
             sys.stderr.write("\r" + str(writeCount))
-        unitLengthString = aarFile[-1].read(4)
+        unit = index1.read(12)
+        if len(unit) == 0:
+            break
+        index2ptr, fileno, offset = struct.unpack(">LLL", unit)
+        if combineFiles and fileno == len(aarFile) - 1:
+            fileno = 0L
+        unit = struct.pack(">LLL", index2ptr, fileno, offset) 
+        writeCount += 1
+        aarFile[0].write(unit)
+        aarFileLength[0] += 12
+    sys.stderr.write("\r" + str(writeCount) + "\n")
+    index1.close()
+    
+    sys.stderr.write("Writing index 2...\n")
+    
+    index2.flush()
+    index2.seek(0)
+    writeCount = 0
+    while 1:
+        if writeCount % 100 == 0:
+            sys.stderr.write("\r" + str(writeCount))
+        unitLengthString = index2.read(4)
         if len(unitLengthString) == 0:
             break
         writeCount += 1
         unitLength = struct.unpack(">L", unitLengthString)[0]
-        unit = aarFile[-1].read(unitLength)
+        unit = index2.read(unitLength)
         aarFile[0].write(unitLengthString + unit)
         aarFileLength[0] += 4 + unitLength
     sys.stderr.write("\r" + str(writeCount) + "\n")
-    sys.stderr.write("Deleting %s\n" % aarFile[-1].name)
-    os.remove(aarFile[-1].name)    
-    aarFile.pop()
-
-sys.stderr.write("Created %i output file(s)\n" % len(aarFile))
-for f in aarFile:
-    f.close
-   
-sys.stderr.write("Done.\n")
+    index2.close()
+    
+    writeCount = 0L
+    
+    if combineFiles:
+        sys.stderr.write("Appending %s to %s\n" % (aarFile[-1].name, aarFile[0].name))
+        aarFile[-1].flush()
+        aarFile[-1].seek(0)
+        aarFile[-1].read(5)
+        headerLength = int(aarFile[-1].read(8))
+        aarFile[-1].read(headerLength)
+    
+        while 1:
+            if writeCount % 100 == 0:
+                sys.stderr.write("\r" + str(writeCount))
+            unitLengthString = aarFile[-1].read(4)
+            if len(unitLengthString) == 0:
+                break
+            writeCount += 1
+            unitLength = struct.unpack(">L", unitLengthString)[0]
+            unit = aarFile[-1].read(unitLength)
+            aarFile[0].write(unitLengthString + unit)
+            aarFileLength[0] += 4 + unitLength
+        sys.stderr.write("\r" + str(writeCount) + "\n")
+        sys.stderr.write("Deleting %s\n" % aarFile[-1].name)
+        os.remove(aarFile[-1].name)    
+        aarFile.pop()
+    
+    sys.stderr.write("Created %i output file(s)\n" % len(aarFile))
+    for f in aarFile:
+        f.close
+       
+    sys.stderr.write("Done.\n")
 
 
 
