2f2489822c0e2f9b169a9588cacf37bcc25e1f7c
Igor Tkach <itkach@aarddict.org>
2008-11-20 23:00:30 -0500

    Make font for sub, sup and ref tags larger

--- aarddict/articleformat.py
+++ aarddict/articleformat.py
@@ -256,8 +256,8 @@ class ArticleFormat:
         buffer.create_tag("i", style = pango.STYLE_ITALIC)
         buffer.create_tag("em", style = pango.STYLE_ITALIC)
         buffer.create_tag("u", underline = True)
-        buffer.create_tag("ref", underline=True, rise=8*pango.SCALE,                           
-                          scale=pango.SCALE_XX_SMALL, foreground='blue')
+        buffer.create_tag("ref", underline=True, rise=6*pango.SCALE,                           
+                          scale=pango.SCALE_X_SMALL, foreground='blue')
         buffer.create_tag("tt", family = 'monospace')        
         
         buffer.create_tag("pos", 
@@ -280,8 +280,8 @@ class ArticleFormat:
         buffer.create_tag("p", pixels_above_lines=3, pixels_below_lines=3)
         buffer.create_tag("div", pixels_above_lines=3, pixels_below_lines=3)
         
-        buffer.create_tag("sup", rise=8*pango.SCALE, scale=pango.SCALE_XX_SMALL)
-        buffer.create_tag("sub", rise=-8*pango.SCALE, scale=pango.SCALE_XX_SMALL)
+        buffer.create_tag("sup", rise=6*pango.SCALE, scale=pango.SCALE_X_SMALL)
+        buffer.create_tag("sub", rise=-6*pango.SCALE, scale=pango.SCALE_X_SMALL)
         
         buffer.create_tag("blockquote", indent = 6)
         buffer.create_tag("cite", style=pango.STYLE_ITALIC, 
