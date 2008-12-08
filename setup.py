6de73bed127f85b0f3826e45d40489e1ebe9f1a6
Igor Tkach <itkach@aarddict.org>
2008-12-07 23:14:17 -0500

    Extend py2exe to include defaults.cfg into library.zip

--- setup.py
+++ setup.py
@@ -4,6 +4,32 @@ try:
     import py2exe
 except:
     print 'py2exe is not installed'
+    py2exe_options = {}
+else:
+    import os
+    import glob
+    from py2exe.build_exe import py2exe as build_exe
+    
+    class Collector(build_exe):
+        def copy_extensions(self, extensions):
+            build_exe.copy_extensions(self, extensions)
+            
+            # Create subdir where the
+            # Python files are collected.
+            full = os.path.join(self.collect_dir, 'aarddict')
+            if not os.path.exists(full):
+                self.mkpath(full)
+                
+            # Copy the files to the collection dir.
+            # Also add the copied file to the list of compiled
+            # files so it will be included in zipfile.
+            for f in glob.glob('aarddict/*.cfg'):
+                name = os.path.basename(f)
+                self.copy_file(f, os.path.join(full, name))
+                self.compiled_files.append(os.path.join('aarddict', name))
+    py2exe_options = {
+        'cmdclass': {'py2exe': Collector}        
+        }
 
 # files to install
 inst_icons_26   = [ 'icons/hicolor/26x26/hildon/aarddict.png' ]
@@ -60,6 +86,7 @@ setup(
         'sdist': {
             'formats': 'zip',
             }
-        }    
+        },
+    **py2exe_options    
 )
 
