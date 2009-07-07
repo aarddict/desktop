2231807acaccdb45ea567fa1e670827ad3067d32
Igor Tkach <itkach@aarddict.org>
2009-07-06 22:24:06 -0400

    Updated dictionary.verify() to report progress

--- aarddict/__init__.py
+++ aarddict/__init__.py
@@ -4,7 +4,7 @@
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3
-# as published by the Free Software Foundation. 
+# as published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -27,61 +27,79 @@ from os import path
 package_dir = path.abspath(path.dirname(__file__))
 
 def main():
-    
+
     usage = "usage: %prog [options] [FILE1] [FILE2] ..."
     parser = optparse.OptionParser(version="%%prog %s" % __version__, usage=usage)
     parser.add_option(
         '-i', '--identify',
-        action='store_true',        
+        action='store_true',
         default=False,
         help='Print identity information for files specified'
         )
     parser.add_option(
         '-v', '--verify',
-        action='store_true',        
+        action='store_true',
         default=False,
         help='Verify dictionary files specified'
-        )    
+        )
     parser.add_option(
         '-d', '--debug',
-        action='store_true',        
+        action='store_true',
         default=False,
         help='Turn on debugging information'
         )
     parser.add_option(
         '-m', '--metadata',
-        action='store_true',        
+        action='store_true',
         default=False,
         help='Print metadata for dictionary files specified'
-        )        
+        )
     options, args = parser.parse_args()
-    
+
     if options.debug:
         logging.getLogger().setLevel(logging.DEBUG)
-    
+
     if options.identify:
         from aarddict import dictionary
         import uuid
         for file_name in args:
             print '%s:' % file_name
-            with open(file_name) as f:            
+            with open(file_name) as f:
                 header = dictionary.Header(f)
                 for name, fmt in dictionary.HEADER_SPEC:
                     value = getattr(header, name)
-                    if name == 'uuid':                        
+                    if name == 'uuid':
                         value = uuid.UUID(bytes=value)
-                    print '\t%s: %s' % (name, value)                                     
-    
+                    print '\t%s: %s' % (name, value)
+
     if options.verify:
         from aarddict import dictionary
         import sys
+        ERASE_LINE = '\033[2K'
+        BOLD='\033[1m'
+        RED = '\033[91m'
+        GREEN = '\033[92m'
+        ENDC = '\033[0m'
+
         for file_name in args:
-            sys.stdout.write('Verifying %s' % file_name) 
             d = dictionary.Dictionary(file_name)
-            if d.verify():
-                sys.stdout.write(': OK\n')
+            try:
+                for progress in d.verify():
+                    sys.stdout.write(ERASE_LINE+'\r')
+                    sys.stdout.write('Verifying %s: %.1f%%' % (file_name, 100*progress))
+                    sys.stdout.flush()
+            except dictionary.VerifyError:
+                sys.stdout.write(ERASE_LINE+'\r')
+                sys.stdout.write(file_name+' ')
+                sys.stdout.write(BOLD+RED+'[CORRUPTED]'+ENDC)
+                sys.stdout.write('\n')
+                sys.stdout.flush()
             else:
-                sys.stdout.write(': CORRUPTED\n')
+                sys.stdout.write(ERASE_LINE+'\r')
+                sys.stdout.write(file_name+' ')
+                sys.stdout.write(BOLD+GREEN+'[OK]'+ENDC)
+                sys.stdout.write('\n')
+                sys.stdout.flush()
 
     if options.metadata:
         from aarddict import dictionary
@@ -89,19 +107,19 @@ def main():
             d = dictionary.Dictionary(file_name)
             print '%s metadata:' % file_name
             print '\n'.join(('\t%s: %s' % item) for item in d.metadata.iteritems())
-                
+
     if options.identify or options.verify or options.metadata:
-        raise SystemExit             
-    
+        raise SystemExit
+
     try:
         import hildon
-    except:        
+    except:
         import ui
         viewer = ui.DictViewer()
     else:
         import hildonui
         viewer = hildonui.HildonDictViewer()
     viewer.main()
-    
+
 if __name__ == '__main__':
     main()
--- aarddict/dictionary.py
+++ aarddict/dictionary.py
@@ -2,7 +2,7 @@
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3
-# as published by the Free Software Foundation. 
+# as published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -19,6 +19,7 @@ import struct
 import logging
 import zlib
 import bz2
+import os
 from bisect import bisect_left
 
 import simplejson
@@ -36,15 +37,15 @@ compression = (zlib.compress,
 decompression = (zlib.decompress,
                  bz2.decompress)
 
-def calcsha1(file_name, offset, chunksize=100000):    
-    with open(file_name, 'rb') as f:    
+def calcsha1(file_name, offset, chunksize=100000):
+    with open(file_name, 'rb') as f:
         f.seek(offset)
-        result = sha1()    
+        result = sha1()
         while True:
             s = f.read(chunksize)
+            if not s: break            
             result.update(s)
-            if not s: break
-        return result.hexdigest()
+            yield (f.tell(), result)
 
 def decompress(s):
     decompressed = s
@@ -55,7 +56,7 @@ def decompress(s):
             pass
         else:
             break
-    return decompressed    
+    return decompressed
 
 def key(s):
     return ucollator.getCollationKey(s).getByteArray()
@@ -70,15 +71,15 @@ class Word(object):
 
     def __unicode__(self):
         return self.unicode
-    
+
     def __repr__(self):
         return self.word
 
     def __cmp__(self, other):
         k1 = key(self.unicode[:len(other.unicode)])
-        k2 = key(other.unicode)        
+        k2 = key(other.unicode)
         return cmp(k1, k2)
-    
+
 def _read(file, pos, fmt):
     file.seek(pos)
     s = file.read(struct.calcsize(fmt))
@@ -89,7 +90,7 @@ def _readstr(file, pos, len_fmt):
     return file.read(strlen)
 
 def _read_index_item(file, offset, itemfmt, itemno):
-    return _read(file, offset + itemno * struct.calcsize(itemfmt), itemfmt)            
+    return _read(file, offset + itemno * struct.calcsize(itemfmt), itemfmt)
 
 def _read_key(file, offset, len_fmt, pos):
     return _readstr(file, offset + pos, len_fmt)
@@ -98,51 +99,51 @@ def _read_article(dictionary, offset, len_fmt, pos):
     decompressed_article = _read_raw_article(dictionary, offset, len_fmt, pos)
     article = to_article(decompressed_article)
     article.dictionary = dictionary
-    return article 
+    return article
 
 def _read_raw_article(dictionary, offset, len_fmt, pos):
-    compressed_article = _readstr(dictionary.file, offset + pos, len_fmt)        
+    compressed_article = _readstr(dictionary.file, offset + pos, len_fmt)
     return decompress(compressed_article)
 
 class WordList(object):
-    
+
     def __init__(self, length, read_index_item, read_key):
         self.length = length
         self.read_index_item = read_index_item
         self.read_key = read_key
-    
+
     def __len__(self):
         return self.length
-    
+
     def __getitem__(self, i):
         if 0 <= i < len(self):
-            key_pos, article_unit_ptr = self.read_index_item(i)            
+            key_pos, article_unit_ptr = self.read_index_item(i)
             key = self.read_key(key_pos)
-            return Word(key)            
+            return Word(key)
         else:
             raise IndexError
-        
+
 class ArticleList(object):
-    
+
     def __init__(self, dictionary, read_index_item, read_key, read_article):
         self.dictionary = dictionary
         self.read_index_item = read_index_item
         self.read_key = read_key
         self.read_article = read_article
-                
+
     def __len__(self):
         return len(self.dictionary)
-    
+
     def __getitem__(self, i):
         if 0 <= i < len(self):
-            key_pos, article_unit_ptr = self.read_index_item(i)            
+            key_pos, article_unit_ptr = self.read_index_item(i)
             key = self.read_key(key_pos)
             article_func = functools.partial(self.read_article, article_unit_ptr)
             article_func.title = key.decode('utf-8')
             article_func.source = self.dictionary
             return article_func
         else:
-            raise IndexError                
+            raise IndexError
 
 class Article(object):
 
@@ -151,16 +152,16 @@ class Article(object):
         self.text = text
         self.tags = [] if tags is None else tags
         self.meta = {} if meta is None else meta
-        self.dictionary = dictionary 
+        self.dictionary = dictionary
 
     def _redirect(self):
         return self.meta.get(u'r', self.meta.get('redirect', u'')).encode('utf8')
-    
-    redirect = property(_redirect)        
+
+    redirect = property(_redirect)
 
     def __repr__(self):
         """
-        >>> a = Article(title=u'a', text=u'Article about a', 
+        >>> a = Article(title=u'a', text=u'Article about a',
         ...             tags=[Tag(u'b', 1, 2)], meta={u'c': u'whatever'})
         >>> a1 = eval(repr(a))
         >>> a1.title
@@ -173,11 +174,11 @@ class Article(object):
         {u'c': u'whatever'}
 
         """
-        return 'Article(title=%r, text=%r, tags=%r, meta=%r)' % (self.title, 
-                                                                 self.text, 
-                                                                 self.tags, 
+        return 'Article(title=%r, text=%r, tags=%r, meta=%r)' % (self.title,
+                                                                 self.text,
+                                                                 self.tags,
                                                                  self.meta)
-    
+
 
 class Tag(object):
 
@@ -201,14 +202,14 @@ class Tag(object):
         {u'href': u'abc'}
 
         """
-        return 'Tag(%r, %r, %r, attributes=%r)'%(self.name, self.start, 
+        return 'Tag(%r, %r, %r, attributes=%r)'%(self.name, self.start,
                                                  self.end, self.attributes)
 
 
 def to_tag(tagtuple):
-    return Tag(tagtuple[0], tagtuple[1], tagtuple[2], 
-               {} if len(tagtuple) < 4 else tagtuple[3])  
-        
+    return Tag(tagtuple[0], tagtuple[1], tagtuple[2],
+               {} if len(tagtuple) < 4 else tagtuple[3])
+
 def to_article(raw_article):
     try:
         articletuple = simplejson.loads(raw_article)
@@ -221,124 +222,124 @@ def to_article(raw_article):
         logging.exception('was trying to load article from string:\n%s', raw_article[:10])
         text = raw_article
         tags = []
-    else:        
-        tags = [to_tag(tagtuple) for tagtuple in tag_list]            
+    else:
+        tags = [to_tag(tagtuple) for tagtuple in tag_list]
     return Article(text=text, tags=tags, meta=meta)
 
-            
+
 HEADER_SPEC = (('signature',                '>4s'), # string 'aard'
-               ('sha1sum',                  '>40s'), #sha1 sum of dictionary file content following signature and sha1 bytes  
+               ('sha1sum',                  '>40s'), #sha1 sum of dictionary file content following signature and sha1 bytes
                ('version',                  '>H'), #format version, a number, current value 1
                ('uuid',                     '>16s'), # dictionary UUID, shared by all volumes of the same dictionary
                ('volume',                   '>H'), # volume number of this this file
                ('of',                       '>H'), # total number of volumes for the dictionary
                ('meta_length',              '>L'), #length of metadata compressed string
                ('index_count',              '>L'), #number of words in the dictionary
-               ('article_offset',           '>L'), #article offset 
+               ('article_offset',           '>L'), #article offset
                ('index1_item_format',       '>4s'), #'>LLL' - represents key pointer, file number and article pointer
                ('key_length_format',        '>2s'), ##'>H' - key length format in index2_item
-               ('article_length_format',    '>2s'), ##'>L' - article length format                              
-               )  
+               ('article_length_format',    '>2s'), ##'>L' - article length format
+               )
 
 def spec_len(spec):
     result = 0
     for name, fmt in spec:
         result += struct.calcsize(fmt)
     return result
-            
+
 class Header(object):
-    
-    def __init__(self, file):    
+
+    def __init__(self, file):
         for name, fmt in HEADER_SPEC:
             s = file.read(struct.calcsize(fmt))
             value, = struct.unpack(fmt, s)
             setattr(self, name, value)
-         
+
     index1_offset = property(lambda self: spec_len(HEADER_SPEC) + self.meta_length)
     index2_offset = property(lambda self: self.index1_offset + self.index_count*struct.calcsize(self.index1_item_format))
-                            
+
 class Dictionary(object):
 
     def __init__(self, file_or_filename, raw_articles=False):
         if isinstance(file_or_filename, file):
             self.file_name = file_or_filename.name
-            close_on_error = False 
+            close_on_error = False
             self.file = file_or_filename
-        else:            
+        else:
             self.file_name = file_or_filename
             close_on_error = True
             self.file = open(file_or_filename, "rb")
-        
+
         header = Header(self.file)
-        
+
         if header.signature != 'aard':
             if close_on_error and self.file:
                 self.file.close()
             raise DictFormatError("%s is not a recognized aarddict dictionary file" % self.file_name)
-        
+
         if header.version != 1:
             if close_on_error and self.file:
                 self.file.close()
             raise DictFormatError("%s is not compatible with this viewer" % self.file_name)
-        
-        self.index_count = header.index_count        
+
+        self.index_count = header.index_count
         self.sha1sum = header.sha1sum
         self.uuid = header.uuid
         self.volume = header.volume
         self.total_volumes = header.of
-        
+
         raw_meta = self.file.read(header.meta_length)
         self.metadata = simplejson.loads(decompress(raw_meta))
-        
-        self.article_count = self.metadata.get('article_count', 
+
+        self.article_count = self.metadata.get('article_count',
                                                self.index_count)
-                
+
         self.index_language = self.metadata.get("index_language", "")
         if isinstance(self.index_language, unicode):
             self.index_language = self.index_language.encode('utf8')
-                        
+
         locale_index_language = Locale(self.index_language).getLanguage()
         if locale_index_language:
             self.index_language = locale_index_language
-            
+
         self.article_language = self.metadata.get("article_language", "")
         if isinstance(self.article_language, unicode):
             self.article_language = self.index_language.encode('utf8')
-        
+
         locale_article_language = Locale(self.index_language).getLanguage()
         if locale_article_language:
             self.article_language = locale_article_language
 
-        read_index_item = functools.partial(_read_index_item, 
-                                                 self.file, 
-                                                 header.index1_offset, 
+        read_index_item = functools.partial(_read_index_item,
+                                                 self.file,
+                                                 header.index1_offset,
                                                  header.index1_item_format)
         read_key = functools.partial(_read_key,
                                      self.file,
                                      header.index2_offset,
                                      header.key_length_format)
-        read_article_func = _read_raw_article if raw_articles else _read_article 
-        read_article = functools.partial(read_article_func, 
-                                         self, 
+        read_article_func = _read_raw_article if raw_articles else _read_article
+        read_article = functools.partial(read_article_func,
+                                         self,
                                          header.article_offset,
                                          header.article_length_format)
         self.words = WordList(self.index_count, read_index_item, read_key)
-        self.articles = ArticleList(self, 
-                                    read_index_item, 
-                                    read_key, 
-                                    read_article)                                
-            
+        self.articles = ArticleList(self,
+                                    read_index_item,
+                                    read_key,
+                                    read_article)
+
     title = property(lambda self: self.metadata.get("title", ""))
     version = property(lambda self: self.metadata.get("version", ""))
     description = property(lambda self: self.metadata.get("description", ""))
-    copyright = property(lambda self: self.metadata.get("copyright", ""))    
+    copyright = property(lambda self: self.metadata.get("copyright", ""))
     license = property(lambda self: self.metadata.get("license", ""))
     source = property(lambda self: self.metadata.get("source", ""))
-    
+
     def __len__(self):
         return self.index_count
-    
-    def __getitem__(self, s):        
+
+    def __getitem__(self, s):
         startword = Word(s)
         pos = bisect_left(self.words, startword)
         try:
@@ -348,19 +349,19 @@ class Dictionary(object):
                 yield self.articles[pos]
                 pos += 1
         except IndexError:
-            raise StopIteration        
+            raise StopIteration
 
     def __contains__(self, s):
         for item in self[s]:
             return True
         return False
-        
+
     def __eq__(self, other):
         return self.key() == other.key()
-    
+
     def __str__(self):
         return self.file_name
-    
+
     def __hash__(self):
         return self.key().__hash__()
 
@@ -368,27 +369,36 @@ class Dictionary(object):
         return self.sha1sum
 
     def verify(self):
-        sha1sum = calcsha1(self.file_name, spec_len(HEADER_SPEC[:2]))
-        return sha1sum == self.sha1sum
+        st_size = os.stat(self.file_name).st_size
+        offset = spec_len(HEADER_SPEC[:2])
+        size = float(st_size - offset)
+        result = None
+        for pos, result in calcsha1(self.file_name, offset):
+            yield pos/size
+        if not result or result.hexdigest() != self.sha1sum:
+            raise VerifyError()            
 
     def close(self):
-        self.file.close()        
-            
+        self.file.close()
+
 class DictFormatError(Exception):
+
     def __init__(self, value):
         self.value = value
-    def __str__(self):
-        return repr(self.value)      
 
-        
+    def __str__(self):
+        return repr(self.value)
+
+class VerifyError(Exception): pass
+
 class DictionaryCollection(list):
-    
+
     def langs(self):
         return set([d.index_language for d in self])
-    
+
     def volumes(self, uuid):
-        return [d for d in self if d.uuid == uuid] 
-    
+        return [d for d in self if d.uuid == uuid]
+
     def lookup(self, start_word, max_from_one_dict=50, uuid=None):
         if uuid:
             dicts = self.volumes(uuid)
@@ -399,4 +409,4 @@ class DictionaryCollection(list):
             for article in dictionary[start_word]:
                 yield article
                 count += 1
-                if count >= max_from_one_dict: break            
+                if count >= max_from_one_dict: break
