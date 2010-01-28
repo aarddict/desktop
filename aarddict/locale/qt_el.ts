0f754fb24cc41d0c09decd437cd25edd22cc89c2
Jennie Petoumenou <epetoumenou@gmail.com>
2010-01-27 23:30:46 -0500

    Add Greek Qt translation for 'Close' dialog button

---
+++ aarddict/locale/qt_el.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- -*- mode: sgml; -*- -->
+<!DOCTYPE TS>
+<TS version="2.0" language="el">
+<context>
+    <name>QDialogButtonBox</name>
+    <message>
+        <source>&amp;Close</source>
+        <translation>&amp;Κλείσιμο</translation>
+    </message>
+</context>
+</TS>
