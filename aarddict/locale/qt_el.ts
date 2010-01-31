d63219c22429e3a06537e3dc4d10aa4f9013ffdb
Igor Tkach <itkach@aarddict.org>
2010-01-30 23:25:48 -0500

    Update deb extended description formatting

--- aarddict/locale/qt_el.ts
+++ aarddict/locale/qt_el.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!-- -*- mode: sgml; -*- -->
 <!DOCTYPE TS>
-<TS version="2.0" language="el">
+<TS version="2.0" language="el_GR">
 <context>
     <name>QDialogButtonBox</name>
     <message>
