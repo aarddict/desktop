702060dc912c3333360e87e88c29a0c5b205b101
Igor Tkach <itkach@aarddict.org>
2010-07-07 22:19:53 -0400

    Add semicolons in aar.js, make sure all vars are declared

--- aarddict/aar.js
+++ aarddict/aar.js
@@ -1,40 +1,40 @@
 function text(node){
-  var result = []
+  var result = [];
   for (var i = 0; i < node.childNodes.length; i++) {
-    c = node.childNodes[i]
-    if (c.nodeType == 3) result.push(c.nodeValue)
-    else result.push(text(c))
+    c = node.childNodes[i];
+    if (c.nodeType == 3) result.push(c.nodeValue);
+    else result.push(text(c));
   }
-  return result.join('')
+  return result.join('');
 }
 
 function headings(level)
 {
-  var headingElements = document.getElementsByTagName("h"+level)
-  result = []
+  var headingElements = document.getElementsByTagName("h"+level);
+  var result = [];
   for (var i=0; i < headingElements.length; i++) {
-    var element = headingElements[i]
-    var elementText = text(element)
-    result.push([elementText, element])
+    var element = headingElements[i];
+    var elementText = text(element);
+    result.push([elementText, element]);
   }
-  return result
+  return result;
 }
 
 function scrollToMatch(text, matchStrength) {
   for (var j=1; j <=6; j++) {
-     var h = headings(j)
+     var h = headings(j);
      for (var k=0; k < h.length; k++) {
-       heading = h[k]
-       headingText = heading[0]
-       matcher.match(headingText, text, matchStrength)
+       var heading = h[k];
+       var headingText = heading[0];
+       matcher.match(headingText, text, matchStrength);
        if (matcher.result) {
-          headingElem = heading[1]
-          headingElem.scrollIntoView(true)
-          return true
+          var headingElem = heading[1];
+          headingElem.scrollIntoView(true);
+          return true;
        }
      }
   }
-  return false
+  return false;
 }
 
 function s(elementId) {
